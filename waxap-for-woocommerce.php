1bdff41711a699076caa77d346ab10768c70f3e5
Daniel Ruiz <druiz7@gmail.com>
2026-05-26 23:17:32 +0200

    chore: bump version to 0.3.4

--- waxap-for-woocommerce.php
+++ waxap-for-woocommerce.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Waxap for WooCommerce
  * Plugin URI:        https://wanotifier.example
  * Description:       Send transactional WhatsApp notifications to your WooCommerce customers. Bring your own number, scan a QR, done.
- * Version:           0.3.3
+ * Version:           0.3.4
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            drappsinfo
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WA_NOTIFIER_VERSION', '0.3.3' );
+define( 'WA_NOTIFIER_VERSION', '0.3.4' );
 define( 'WA_NOTIFIER_FILE', __FILE__ );
 define( 'WA_NOTIFIER_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WA_NOTIFIER_URL', plugin_dir_url( __FILE__ ) );
